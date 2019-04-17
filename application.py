c46edf249b74785595bad66b969a6d928a9b6a8f
Vartic, Mihai (Nokia - RO/Timisoara) <mihai.vartic@nokia.com>
2019-04-17 09:09:41 +0300

    roll back route

--- application.py
+++ application.py
@@ -6,7 +6,6 @@ import uuid
 
 import process_data as prodata
 # Initialize the Flask application
-@app.route("/")
 app = Flask(__name__)
 
 # This is the path to the templates directory
@@ -21,7 +20,7 @@ def allowed_file(filename):
 		   filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 
-# @app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def comp():
 	msg= None
 	if request.method == 'POST':
